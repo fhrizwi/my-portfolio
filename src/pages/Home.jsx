1fd489b7101f491eb14ed7ae22e14d3efc88b6c2
Faizul Haque Rizwi <faizulhaque2002@gmail.com>
2025-05-08 14:29:49 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Header from '../components/Header';
 
 export default function Home() {
   return (
     <div>
-      {/* <Header /> */}
-      <p>Welcome to Home Page</p>
+      <p>Welcome to my website</p>
     </div>
   );
 }
