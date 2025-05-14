796d4633e054bae02571818b0ea0f8a4dff3baad
fhrizwi <faizulhaque2002@gmail.com>
2025-05-15 01:09:44 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,7 @@
-import Snowfall from 'react-snowfall';
 import Hero from '../components/Hero';
 
 export default function Home() {
   return (
-    // <div className="relative overflow-hidden">
-    //   {/* Snowfall in background */}
-    //   <div className="absolute inset-0 z-0 h-screen pointer-events-none opacity-50">
-    //     <Snowfall color="green" snowflakeCount={200} />
-    //   </div>
-
-    //   {/* Main content above snowfall */}
-      // <div className="relative z-10">
       <div>
         <Hero />
       </div>
